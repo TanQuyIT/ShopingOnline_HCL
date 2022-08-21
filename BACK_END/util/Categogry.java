e6fa381aa22ed744c9fff8a517aa3faa07f2466c
TanQuyIT <95470678+TanQuyIT@users.noreply.github.com>
2022-08-21 23:11:50 +0700

    Update hibernate

---
+++ BACK_END/util/Categogry.java
@@ -0,0 +1,42 @@
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categogry")
+public class Categogry {
+    @Id
+    Integer id;
+
+    @Column
+    String name;
+
+    public Categogry(){
+        super();
+    }
+
+    public Categogry(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
